4917a92a0a38b7482d7c03958a9cf7030bb5aab0
Rajnish379 <rajinish77@gmail.com>
2024-07-19 14:09:00 -0500

    Final commit for frontend

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import { Routes, Route, useLocation } from "react-router-dom";
+function App() {
+  const location = useLocation();
+  const noNavbar =
+    location.pathname === "/register" || location.pathname === "/";
+
+  return (
+    <>
+      {noNavbar ? (
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      ) : (
+        <Navbar
+          content={
+            <Routes>
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          }
+        />
+      )}
+    </>
+  );
+}
+
+export default App;
